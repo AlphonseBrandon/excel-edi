c3f7d3b6c9ab2ea35f7a6d67499451aaac6be0b8
AlphonseBrandon <alphonse.brand@gmail.com>
2024-04-10 17:13:45 +0200

    Add code to generate EDI 834 file from Excel data

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 import pandas as pd
-import pyx12
 
 # Load Excel data (assuming you have an 'enrollment.xlsx' file)
 excel_file = '../data/excel/enrollment.xlsx'
 df = pd.read_excel(excel_file)
 
+
 # Create an EDI 834 segment
 def create_edi_segment(member_id, first_name, last_name, dob, gender, email, start_date, coverage_type):
-    return f"NM1*IL*1*{last_name}*{first_name}***34*{member_id}~"
+    return (
+        f"NM1*IL*1*{last_name}*{first_name}***34*{member_id}~"
+    )
+
 
 # Generate EDI content
 edi_content = []
@@ -25,5 +28,3 @@ with open('../data/edi/enrollment_834.txt', 'w') as f:
     f.write(edi_file)
 
 print("EDI 834 file created successfully!")
-
-# You can now send 'enrollment_834.txt' to the relevant system.
