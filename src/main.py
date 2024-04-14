65088280a9212ab2844921a463e2d834e0391c5a
AlphonseBrandon <alphonse.brand@gmail.com>
2024-04-15 01:47:28 +0200

    Generate EDI 834 transaction files for each patient

--- src/main.py
+++ src/main.py
@@ -1,30 +1,84 @@
 import pandas as pd
 
-# Load Excel data (assuming you have an 'enrollment.xlsx' file)
-excel_file = '../data/excel/enrollment.xlsx'
+# Define the path to the Excel file
+excel_file = 'patient_data.xlsx'
+
+# Load the Excel file into a DataFrame
 df = pd.read_excel(excel_file)
 
+# Iterate over the rows in the DataFrame
+for index, row in df.iterrows():
+    # Create a new EDI 834 transaction file for each patient
+    with open(f'834_{index}.X12', 'w') as f:
+        # Write the ISA segment
+        f.write(
+            'ISA*00*          *00*          *ZZ*SENDERID       *ZZ*RECEIVERID     '
+            '*030101*1253*U*00401*000000001*0*P*>\n'
+        )
 
-# Create an EDI 834 segment
-def create_edi_segment(member_id, first_name, last_name, dob, gender, email, start_date, coverage_type):
-    return (
-        f"NM1*IL*1*{last_name}*{first_name}***34*{member_id}~"
-    )
+        # Write the GS segment
+        f.write('GS*BE*SENDERCODE*RECEIVERCODE*19991231*0802*1*X*004010X095A1\n')
 
+        # Write the ST segment
+        f.write('ST*834*0001\n')
 
-# Generate EDI content
-edi_content = []
-for _, row in df.iterrows():
-    edi_segment = create_edi_segment(
-        row['Member ID'], row['First Name'], row['Last Name'],
-        row['Date of Birth'], row['Gender'], row['Email Address'],
-        row['Plan Start Date'], row['Coverage Type']
-    )
-    edi_content.append(edi_segment)
+        # Write the BGN segment
+        f.write('BGN*00*ReferenceIdentification*19991231*Time*TimeZoneCode**2\n')
 
-# Create the EDI file
-edi_file = '\n'.join(edi_content)
-with open('../data/edi/enrollment_834.txt', 'w') as f:
-    f.write(edi_file)
+        # Write the REF segment
+        f.write('REF*38*ApplicationReceiverCode\n')
 
-print("EDI 834 file created successfully!")
+        # Write the N1 segment
+        f.write('N1*P5*PlanSponsorName*FI*PlanSponsorTaxId\n')
+
+        # Write the INS segment
+        f.write(
+            f'INS*{row["RelationshipCode"]}*{row["MemberActionCode"]}**'
+            f'{row["MaintenanceTypeCode"]}**{row["MaintenanceReasonCode"]}**'
+            f'{row["BenefitStatusCode"]}**{row["MedicareStatusCode"]}**'
+            f'{row["COBRACode"]}**{row["EmploymentStatusCode"]}**'
+            f'{row["StudentStatusCode"]}**{row["HandicapIndicator"]}**'
+            f'{row["DateOfBirth"]}\n'
+        )
+
+        # Write the REF segment
+        f.write(f'REF*0F*{row["MemberPolicyNumber"]}\n')
+
+        # Write the DTP segment
+        f.write(f'DTP*336*D8*{row["CoverageStartDate"]}\n')
+
+        # Write the NM1 segment
+        f.write(
+            f'NM1*IL*1*{row["LastName"]}*{row["FirstName"]}**{row["MiddleName"]}**'
+            f'{row["NamePrefix"]}**{row["NameSuffix"]}\n'
+        )
+
+        # Write the N3 segment
+        f.write(f'N3*{row["AddressLine1"]}*{row["AddressLine2"]}\n')
+
+        # Write the N4 segment
+        f.write(f'N4*{row["City"]}*{row["State"]}*{row["PostalCode"]}*{row["CountryCode"]}\n')
+
+        # Write the DMG segment
+        f.write(
+            f'DMG*D8*{row["DateOfBirth"]}*{row["GenderCode"]}**{row["MaritalStatusCode"]}**'
+            f'{row["RaceOrEthnicityCode"]}**{row["CitizenshipStatusCode"]}**{row["CountryCode"]}\n'
+        )
+
+        # Write the HD segment
+        f.write(f'HD**{row["InsuranceLineCode"]}**{row["PlanCoverageDescription"]}\n')
+
+        # Write the DTP segment
+        f.write(f'DTP*348*D8*{row["CoverageStartDate"]}\n')
+
+        # Write the SE segment
+        f.write('SE*22*0001\n')
+
+        # Write the GE segment
+        f.write('GE*1*1\n')
+
+        # Write the IEA segment
+        f.write('IEA*1*000000001\n')
+
+# Print a success message
+print("EDI 834 transaction files were successfully generated for each patient.")
